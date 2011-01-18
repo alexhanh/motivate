5742e55babd8619b54a308d0ef3063c093d00cc1
Alexander Hanhikoski <alexander.hanhikoski@gmail.com>
2011-01-18 03:59:58 +0200

    removed vendor, added i18n for jQuery ui datepicker and bunch of other stuff

--- vendor/ruby/1.9.1/gems/faker-0.9.2/lib/faker/phone_number.rb
+++
@@ -1,9 +0,0 @@
-module Faker
-  class PhoneNumber
-    class << self
-      def phone_number
-        Faker.numerify(I18n.translate('phone_number.formats').rand)
-      end
-    end
-  end
-end
--- vendor/ruby/1.9.1/gems/rspec-2.0.0.beta.22/lib/rspec/version.rb
+++
@@ -1,5 +0,0 @@
-module RSpec # :nodoc:
-  module Version # :nodoc:
-    STRING = '2.0.0.beta.22'
-  end
-end
